a5a149fb241ce55b60cdca1ed5a3861ddd36c722
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-04-16 01:21:46 +0530

    Add files via upload

---
+++ Segment-Tree.cpp
@@ -0,0 +1,34 @@
+// Building Segment Tree from an Array
+// Left Child: 2*index
+// Right Chid: 2*index+1
+// Indexing Starts from 1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void buildTree(int *a, int *tree, int start, int end, int index){
+
+	if(start==end){
+		tree[index]=a[start];
+		return;
+	}
+	int mid=(start+end)/2;
+	buildTree(a,tree,start,mid,2*index);
+	buildTree(a,tree,mid+1,end,2*index+1);
+	tree[index]=tree[2*index]+tree[2*index+1];
+}
+
+int main(){
+
+	int a[]={1,2,3,4,5,6,7,8,9};
+	int n=sizeof(a)/sizeof(int);
+
+	int *tree=new int[2*n-1];
+
+	buildTree(a,tree,0,n-1,1);
+
+	for(int i=1;i<2*n;i++)
+		cout<<tree[i]<<" ";
+
+	return 0;
+}
